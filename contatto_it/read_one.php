5fa89fc97dbf297031db715bc4af02883b1f522e
gselvatico <77325565+gselvatico@users.noreply.github.com>
2023-04-05 18:56:24 +0200

    rimosso chiave da read categorie e contatto read_one

--- contatto_it/read_one.php
+++ contatto_it/read_one.php
@@ -13,13 +13,13 @@ include_once '../config/apikey.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
-if ($data->api_key != ApiKey::$apiKey) {
-    http_response_code(403);  
-    echo json_encode(
-        array("message" => "Chiave sbagliata")
-    ); 
-    return;
-} 
+// if ($data->api_key != ApiKey::$apiKey) {
+//     http_response_code(403);  
+//     echo json_encode(
+//         array("message" => "Chiave sbagliata")
+//     ); 
+//     return;
+// } 
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
