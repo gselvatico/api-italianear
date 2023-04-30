da56bc1206ae8a6ec5e83fd074f1f2d4d6132f05
gselvatico <77325565+gselvatico@users.noreply.github.com>
2023-04-30 18:10:25 +0200

    c_rating aggiustamenti

--- objects/c_rating.php
+++ objects/c_rating.php
@@ -23,7 +23,7 @@ class C_rating{
         $this->conn = $db;
         }
 
-function readOne(){
+    function readOne(){
           // query to read single record
         $query = "SELECT											
                     p.c_rating_id,
@@ -58,126 +58,118 @@ function readOne(){
                 $this->createddate = $row['createddate']; 
                 $this->lastmodified = $row['lastmodified']; 
             }  
-}
-
-function readAvg(){
-     $query = "SELECT		
-                COUNT(p.c_rating_id) rating_count,
-                p.contatto_id,	
-                AVG(p.c_rating) rating_avg,
-				MAX(p.lastmodified)  rating_last
-            FROM
-                " . $this->table_name . " p              
-            WHERE
-                p.contatto_id = ?
-            GROUP BY 
-                p.contatto_id";
-  
-    // prepare query statement
-    $stmt = $this->conn->prepare( $query );
-  
-    // bind id of product to be updated
-    $stmt->bindParam(1, $this->contatto_id);
-  
-    // execute query
-    $stmt->execute();
-  
-    // get retrieved row
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    if($row!=null){
-    // set values to object properties
-    $this->rating_count= $row['rating_count'];
-	 $this->contatto_id= $row['contatto_id'];
-     $this->rating_avg= $row['rating_avg'];
-	 $this->rating_last= $row['rating_last'];
     }
-}
 
-function create(){  
-    // query to insert record
-    $query = "INSERT INTO
-                " . $this->table_name . "
-            SET
-                contatto_id=:contatto_id,
-                utente_id=:utente_id,
-                c_rating=:c_rating,
-                description=:description,	       
-				createddate=:createddate";
-    // prepare qcreateddate,uery
-    $stmt = $this->conn->prepare($query);
-  
-    // sanitize
-    $this->email=htmlspecialchars(strip_tags($this->description));   
-   
-    // bind values
-	$stmt->bindParam(":contatto_id", $this->contatto_id);
-    $stmt->bindParam(":utente_id", $this->utente_id);
-	$stmt->bindParam(":c_rating", $this->c_rating);
-	$stmt->bindParam(":description", $this->description);
-	$stmt->bindParam(":createddate", $this->createddate);	
-	
-    // execute query
-    if($stmt->execute()){
-        return true;
-    }
-  
-    return false;
-  
-}
-
-function update(){  
-    // update query
-    $query = "UPDATE " . $this->table_name . "
-            SET               
-                c_rating=:c_rating,
-                description=:description			
-            WHERE contatto_id = :contatto_id
-            AND utente_id= :utente_id";
-  
-    // prepare query statement
-    $stmt = $this->conn->prepare($query);
-  
-    // sanitize
-    $this->email=htmlspecialchars(strip_tags($this->description));  
-  
-    // bind new values
-  
-
-    $stmt->bindParam(":c_rating", $this->c_rating);
-    $stmt->bindParam(":description", $this->description);
-	$stmt->bindParam(":contatto_id", $this->contatto_id);
-	$stmt->bindParam(":utente_id", $this->utente_id);
-	
-    // execute the query
-    if($stmt->execute()){
-        return true;
-    }
-  
-    return false;
-}
+    function readAvg(){
+        $query = "SELECT		
+                    COUNT(p.c_rating_id) rating_count,
+                    p.contatto_id,	
+                    AVG(p.c_rating) rating_avg,
+                    MAX(p.lastmodified)  rating_last
+                FROM
+                    " . $this->table_name . " p              
+                WHERE
+                    p.contatto_id = :contatto_id
+                GROUP BY 
+                    p.contatto_id";
     
-function delete(){
-  
-    // delete query
-    $query = "DELETE FROM " . $this->table_name . 
-            " WHERE contatto_id = :contatto_id
-            AND utente_id = :utente_id";
-  
-    // prepare query
-    $stmt = $this->conn->prepare($query);  
-  
-    // bind id of record to delete
-    $stmt->bindParam(":utente_id", $this->utente_id);
-    $stmt->bindParam(":contatto_id", $this->contatto_id);
-
-  
-    // execute query
-    if($stmt->execute()){
-        return true;
+        $stmt = $this->conn->prepare( $query );
+    
+        $stmt->bindParam(":contatto_id", $this->contatto_id);
+    
+        $stmt->execute();
+    
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($row!=null){
+            $this->rating_count= $row['rating_count'];
+            $this->contatto_id= $row['contatto_id'];
+            $this->rating_avg= $row['rating_avg'];
+            $this->rating_last= $row['rating_last'];
+        }
     }
-  
-    return false;
-}
+
+    function create(){  
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    contatto_id=:contatto_id,
+                    utente_id=:utente_id,
+                    c_rating=:c_rating,
+                    description=:description,	       
+                    createddate=:createddate";
+
+        $stmt = $this->conn->prepare($query);
+    
+        $this->email=htmlspecialchars(strip_tags($this->description));   
+    
+        $stmt->bindParam(":contatto_id", $this->contatto_id);
+        $stmt->bindParam(":utente_id", $this->utente_id);
+        $stmt->bindParam(":c_rating", $this->c_rating);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":createddate", $this->createddate);	
+        
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;    
+    }
+
+    function update(){  
+        // update query
+        $query = "UPDATE " . $this->table_name . "
+                SET               
+                    c_rating=:c_rating,
+                    description=:description			
+                WHERE contatto_id = :contatto_id
+                AND utente_id= :utente_id";
+    
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->email=htmlspecialchars(strip_tags($this->description));  
+    
+        // bind new values
+    
+
+        $stmt->bindParam(":c_rating", $this->c_rating);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":contatto_id", $this->contatto_id);
+        $stmt->bindParam(":utente_id", $this->utente_id);
+        
+        // execute the query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;
+    }
+    
+    function delete(){
+    
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . 
+                " WHERE contatto_id = :contatto_id
+                AND utente_id = :utente_id";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);  
+    
+        // bind id of record to delete
+        $stmt->bindParam(":utente_id", $this->utente_id);
+        $stmt->bindParam(":contatto_id", $this->contatto_id);
+
+    
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;
+    }
 }
 ?>
 
