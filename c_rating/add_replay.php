e09de42b5504a02d0395d352728fc7b3af1d85a2
giovanni selvatico <g.selvatico@mondoalternative.com>
2024-03-11 19:31:25 +0100

    add_replay

---
+++ c_rating/add_replay.php
@@ -0,0 +1,58 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// include database and object files
+include_once '../config/database.php';
+include_once '../config/apikey.php';
+include_once '../objects/c_rating.php';
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+if ($data->api_key != ApiKey::$apiKey) {
+    http_response_code(403);  
+    echo json_encode(
+        array("message" => "Chiave sbagliata")
+    ); 
+    return;
+} 
+// get database connection
+$database = new Database();
+if(isset($data->isTest) && $data->isTest)
+{
+    $db = $database->getTestConnection();
+}else {
+    $db = $database->getConnection();  
+}
+  
+// prepare c_rating object
+$c_rating = new C_rating($db);
+  
+// set ID property of c_rating to be edited
+$c_rating->c_rating_id = $data->rating_id;
+$c_rating->replay = $data->replay;
+
+// update the c_rating insertin replay
+if($c_rating->addReplay()){
+  
+    // set response code - 200 ok
+    http_response_code(200);
+  
+    // tell the user
+    echo json_encode(array("message" => "c_rating was updated."));
+}
+  
+// if unable to update the c_rating, tell the user
+else{
+  
+    // set response code - 503 service unavailable
+    http_response_code(503);
+  
+    // tell the user
+    echo json_encode(array("message" => "Unable to update c_rating."));
+}
+?>
